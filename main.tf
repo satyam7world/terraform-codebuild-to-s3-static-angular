2e9f2c18667eb4f0be535bc72f9b77cdec6abda4
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-06-12 11:33:45 +0530

    git clone with terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
-resource "aws_codecommit_repository" "emi_repo" {
-  repository_name = "emi-calculator-repo"
-  default_branch = "master"
-  description = "Emi Calculator is a emi calculator built on angular"
+
+# Step 1 - Clone the git repo to this local computer
+resource "null_resource" "emi_git_clone" {
+  provisioner "local-exec" {
+    command = "git clone https://github.com/satyam7world/emi-calculator-angular.git"
+  }
 }
+
+# resource "aws_s3_bucket" "emi_bucket" {
+#   bucket = "emi-calculator-tf"
+# }
+
+# resource "aws_codecommit_repository" "emi_repo" {
+#   repository_name = "emi-calculator-repo"
+#   default_branch = "master"
+#   description = "Emi Calculator is a emi calculator built on angular"
+#  }
