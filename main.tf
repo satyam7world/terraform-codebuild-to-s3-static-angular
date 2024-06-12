4b1acbd3237075b4925b97a74cdad1a0af684505
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-06-12 13:26:55 +0530

    git to s3 working

--- main.tf
+++ main.tf
@@ -30,7 +30,15 @@ resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
 resource "aws_s3_bucket_acl" "bucket_acl" {
   depends_on = [aws_s3_bucket.emi_bucket, aws_s3_bucket_public_access_block.bucket_public_access]
   bucket     = aws_s3_bucket.emi_bucket.id
-  acl = "public-read"
+  acl        = "public-read"
+}
+
+resource "aws_s3_bucket_object" "copy_emi_angular" {
+  bucket   = aws_s3_bucket.emi_bucket.id
+  for_each = fileset("./emi-calculator-angular/", "**/*")
+  key      = each.value
+  source = "./emi-calculator-angular/${each.value}"
+  etag =  filemd5("./emi-calculator-angular/${each.value}")
 }
 
 
