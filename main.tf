dfb2572cde29007eaa0594c7651f8d870edba0f5
Satyam <58844449+satyam7world@users.noreply.github.com>
2024-06-12 12:59:40 +0530

    s3 bucket

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 # Step 1 - Clone the git repo to this local computer
 resource "null_resource" "emi_git_clone" {
   provisioner "local-exec" {
@@ -6,8 +5,39 @@ resource "null_resource" "emi_git_clone" {
   }
 }
 
-# resource "aws_s3_bucket" "emi_bucket" {
-#   bucket = "emi-calculator-tf"
+resource "aws_s3_bucket" "emi_bucket" {
+  bucket = "emi-calculator-tf"
+  tags = {
+    Name : "Emi Calculator Bucket"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+  bucket = aws_s3_bucket.emi_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+  bucket                  = aws_s3_bucket.emi_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [aws_s3_bucket.emi_bucket, aws_s3_bucket_public_access_block.bucket_public_access]
+  bucket     = aws_s3_bucket.emi_bucket.id
+  acl = "public-read"
+}
+
+
+
+# resource "aws_s3_bucket_policy" "bucket_policy" {
+#   bucket = aws_s3_bucket.emi_bucket.id
+#   policy = ""
 # }
 
 # resource "aws_codecommit_repository" "emi_repo" {
